d8d90c0a28520870b65ed43e692f48179ed6cc60
gopal-iosdev <gopal.iosdev@gmail.com>
2026-07-29 18:46:45 -0400

    Added introduction page

---
+++ unowned-self-swift/UnOwnedSelf_vs_Others.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ # Unowned Self vs Others
+
+ A hands-on companion to the blog post ["Unowned Self in Swift: Why and When to Use It"](https://unownedself.com/posts/unowned-self-swift/).
+
+ ## Contents
+ - [Self Type](Self%20Type) - `Self` as a covariant return type
+ - [self](self) - the strong reference cycle
+ - [unowned self](unowned%20self) - breaking the cycle, unsafely
+ - [weak self](weak%20self) - breaking the cycle, safely
+
+ ---
+ */
+
+//: [Next](@next)
